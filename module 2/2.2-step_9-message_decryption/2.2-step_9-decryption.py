486281d2fa2a2e5002ba2778d4c96bf56acb6420
Ivan Artemev <jhnar7@gmail.com>
2022-01-31 21:01:37 +0500

    decryption

---
+++ module 2/2.2-step_9-message_decryption/2.2-step_9-decryption.py
@@ -0,0 +1,20 @@
+from simplecrypt import encrypt, decrypt
+import os
+def decryption():
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+
+    with open(os.path.join(current_dir, 'encrypted.bin'), "rb") as inp:
+        encrypted = inp.read()
+
+    with open(os.path.join(current_dir, 'passwords.txt'), "rb") as inp:
+        passwords = inp.read().split()
+
+    for i in passwords:
+        try:
+            plaintext = decrypt(i, encrypted)
+            print(plaintext)
+        except: 
+            print (i, " not correct")
+
+if __name__ == "__main__":
+    decryption()
